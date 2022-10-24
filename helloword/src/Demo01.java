d3985cb20e8f5632b23699d8a836816c9de2364b
Pony-xiao-yiyang <56623242+Pony-xiao-yiyang@users.noreply.github.com>
2022-10-24 23:13:39 +0800

    Java学习

--- helloword/src/Demo01.java
+++ helloword/src/Demo01.java
@@ -1,10 +1,14 @@
 /**
- * @description:Java学习的第一天
+ * @description:文档注释
  * @author:
  * @time: 2022/10/23 19:48
  */
 public class Demo01 {
+    /**
+     * 多行注释
+     */
     public static void main(String[] args) {
+        //单行注释  输出hello,world
         System.out.println("hello,world");
     }
 }
