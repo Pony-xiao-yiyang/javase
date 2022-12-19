c0916567a5e17e357931642ac6f1f6a6d65d31db
Pony-xiao-yiyang <56623242+Pony-xiao-yiyang@users.noreply.github.com>
2022-12-19 19:27:20 +0800

    Java学习

---
+++ JavaSEBasic/src/javadoc/Demo01.java
@@ -0,0 +1,28 @@
+package javadoc;
+
+
+/**
+ * javaDoc
+ *      1.Javadoc命令用来生成自己的API文档
+ *      2参数信息
+ * @description:    描述类的情况
+ * @author: 作者
+ * @time: 2022/12/19 18:14
+ * @version    版本号
+ * @since      指明需要最早使用的jdk版本
+ * @return     返回值情况
+ */
+public class Demo01 {
+    String name;
+
+    /**
+     *
+     * @param name  参数值
+     * @return  返回值情况
+     * @throws Exception    抛出异常情况
+     */
+    public String test(String name) throws Exception{
+        return name;
+
+    }
+}
